cb0a5f45a3d79cfc08bf05fdfd2a11ffc2988aa4
Tyler Addison <taddison3@icloud.com>
2023-11-01 13:35:32 -0400

    Fix error for budget lacking dispatch

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,13 +3,18 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
+        });
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £ </span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
